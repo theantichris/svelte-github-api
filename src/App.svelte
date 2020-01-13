3fc7c0f62bae04744753da61decee0393257110b
Christopher <chris@theantichris.com>
2020-01-13 15:29:04 -0500

    removed prop

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,5 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello World!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
