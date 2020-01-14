cfd5f44be158b06225cd06edfa1a1dc625da45bc
Christopher Lamm <chris@theantichris.com>
2020-01-14 18:39:20 -0500

    added test for fetch event

--- tests/UserSearch.spec.js
+++ tests/UserSearch.spec.js
@@ -1,9 +1,27 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/svelte'
+import { render, fireEvent, act } from '@testing-library/svelte'
 import UserSearch from '../src/UserSearch.svelte'
 
 test('shows header when rendered', () => {
   const { getByText } = render(UserSearch)
 
   expect(getByText('Search for Users')).toBeInTheDocument()
+})
+
+test('searches GitHub for user', async () => {
+  fetch.mockResponseOnce(JSON.stringify({
+    "login": "theantichris",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/1486502?v=4"
+  }))
+
+  const { getByText, getByPlaceholderText } = render(UserSearch)
+
+  const input = getByPlaceholderText('search...')
+  await fireEvent.input(input, { target: { value: 'theantichris' } })
+
+  const button = getByText('Search')
+  await fireEvent.click(button)
+
+  expect(fetch.mock.calls.length).toEqual(1)
+  expect(fetch.mock.calls[0][0]).toEqual('https://api.github.com/users/theantichris')
 })
